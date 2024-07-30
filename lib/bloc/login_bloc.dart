ca697e949f165dcce857cd0715c6ab2e6f2ee6d0
Huzaifa atta <huzaifaatta8152@gmail.com>
2024-07-30 15:33:52 +0500

    1.Building Bloc mvvm

--- lib/bloc/login_bloc.dart
+++ lib/bloc/login_bloc.dart
@@ -1,4 +1,5 @@
 import 'package:bloc/bloc.dart';
+import 'package:bloc_mvvm_2/Repository/Auth/login_repository.dart';
 import 'package:bloc_mvvm_2/Utils/enum.dart';
 import 'package:equatable/equatable.dart';
 
@@ -6,9 +7,11 @@ part 'login_event.dart';
 part 'login_state.dart';
 
 class LoginBloc extends Bloc<LoginEvent, LoginState> {
+  LoginRepository loginRepository = LoginRepository();
   LoginBloc() : super(const LoginState()) {
       on<EmailChanged>(_onEmailChanged);
       on<PasswordChanged>(_onPasswordChanged);
+      on<LoginButton>(_loginButton);
   }
 
   //functions for emit states
@@ -18,6 +21,23 @@ class LoginBloc extends Bloc<LoginEvent, LoginState> {
    }
    void _onPasswordChanged(PasswordChanged event,Emitter<LoginState>emit){
     emit(state.copywith(password: event.password));
+  }
+   void  _loginButton(LoginButton event,Emitter<LoginState>emit)async{
+    Map data= {
+      "email": state.email,
+      "password": state.password
+    };
+
+    await loginRepository.loginApi(data).then((value){
+        if(value.error.isNotEmpty){
+          print('successful');
+        }else{
+          state.copywith(error: value.error.toString());
+        }
+    }).onError((error,stackTrace){
+      emit(state.copywith(error: error.toString()));
+    });
+
   }
 
 }
