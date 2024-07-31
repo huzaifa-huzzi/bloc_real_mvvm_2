8f4332bd86ade595e80d8e38ba8b2902ea5a765b
Huzaifa atta <huzaifaatta8152@gmail.com>
2024-07-31 18:12:52 +0500

    1.Building Bloc mvvm

--- lib/Services/storage/local_storage.dart
+++ lib/Services/storage/local_storage.dart
@@ -9,5 +9,14 @@ class LocalStorage{
        await storage.write(key: key, value: value);
        return true;
      }
+     // reading this value
+    Future<dynamic> readValue(String key)async{
+       return await storage.read(key: key);
+    }
+     //for clearing the values
+    Future<bool> clearValue(String key)async{
+       await storage.delete(key: key);
+       return true;
+    }
 
 }
