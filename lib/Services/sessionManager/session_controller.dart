385a67b918b368d02f77600fbd2c687a408f3dca
Huzaifa atta <huzaifaatta8152@gmail.com>
2024-08-01 15:23:48 +0500

    1.Building Bloc mvvm

--- lib/Services/sessionManager/session_controller.dart
+++ lib/Services/sessionManager/session_controller.dart
@@ -1,16 +1,26 @@
+import 'dart:convert';
 
-
+import 'package:bloc_mvvm_2/Services/storage/local_storage.dart';
+import 'package:bloc_mvvm_2/models/user/user_model.dart';
 
 class SessionManager {
   static final SessionManager _session = SessionManager._internal();
 
-
+  final LocalStorage localStorage = LocalStorage();
+  UserModel user = UserModel();
+  bool? isLogin;
 
   factory SessionManager(){
     return _session;
   }
 
   SessionManager._internal(){
+  }
+
+  Future<void> saveUserInPreferences(dynamic user) async{
+    localStorage.setValue('token',jsonEncode(user));
+    localStorage.setValue('isLogin', 'true');
+
   }
 
 
