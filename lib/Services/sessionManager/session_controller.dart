224d813cc86ab9816ae1caa29adf78543629b1ba
Huzaifa atta <huzaifaatta8152@gmail.com>
2024-08-01 15:28:25 +0500

    1.Building Bloc mvvm

--- lib/Services/sessionManager/session_controller.dart
+++ lib/Services/sessionManager/session_controller.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:bloc_mvvm_2/Services/storage/local_storage.dart';
 import 'package:bloc_mvvm_2/models/user/user_model.dart';
+import 'package:flutter/cupertino.dart';
 
 class SessionManager {
   static final SessionManager _session = SessionManager._internal();
@@ -17,12 +18,30 @@ class SessionManager {
   SessionManager._internal(){
   }
 
+
+  // for saving user
+
   Future<void> saveUserInPreferences(dynamic user) async{
     localStorage.setValue('token',jsonEncode(user));
     localStorage.setValue('isLogin', 'true');
 
   }
 
+  //now getting them
 
+ Future<void> getUserFromPreferences()async{
+
+    try{
+      var userData = await localStorage.readValue('token');
+      var isLogin = await localStorage.readValue('isLogin');
+      if(userData.isNotEmpty){
+        SessionManager().user = UserModel.fromJson(jsonDecode(userData));
+      }
+      SessionManager().isLogin = isLogin == 'true' ? true : false ;
+    }catch(e){
+      debugPrint(e.toString());
+    }
+
+ }
 
 }
