c58fd9dfb8ffa227844d84eddddf5793f1a90928
Huzaifa atta <huzaifaatta8152@gmail.com>
2024-08-01 16:16:37 +0500

    1.Building Bloc mvvm

--- lib/models/Movies/movies.dart
+++ lib/models/Movies/movies.dart
@@ -1,20 +1,24 @@
-
-
-import 'dart:nativewrappers/_internal/vm/lib/core_patch.dart';
-
 import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'movies.freezed.dart';
+part 'movies.g.dart';
+
+
+
+
+
 
 @freezed
 class MoviesModel with _$MoviesModel {
 
   factory MoviesModel({
-    @Default('') int total,
+    @Default('') @JsonKey(name: 'total') String total,
     @Default(0)int page,
     @Default(0)int pages,
     @Default([]) @JsonKey(name:'tv_shows') List<TvShows> tvShow,
 }) = _MoviesModel;
 
-   factory MoviesModel.fromJson(Map<dynamic,dynamic>json) => _$MoviesModelFromJson(json);
+   factory MoviesModel.fromJson(Map<String,dynamic>json) => _$MoviesModelFromJson(json);
 
 }
 
@@ -23,14 +27,14 @@ class MoviesModel with _$MoviesModel {
 class TvShows with _$TvShows {
 
   factory TvShows({
-     @JsonKey(name: 'name') @Default('') dynamic name ,
-     @JsonKey(name: 'permalink') @Default('') dynamic permalink,
-     @JsonKey(name: 'end_date') @Default('') dynamic endDate,
-     @JsonKey(name: 'network') @Default('') dynamic network,
-     @JsonKey(name: 'image_thumbnail_path') @Default('') dynamic imageThumbnailPath,
-     @JsonKey(name: 'status') @Default('') dynamic status,
+     @JsonKey(name: 'name') @Default('') String name ,
+     @JsonKey(name: 'permalink') @Default('') String permalink,
+     @JsonKey(name: 'end_date') @Default('') String endDate,
+     @JsonKey(name: 'network') @Default('') String network,
+     @JsonKey(name: 'image_thumbnail_path') @Default('') String imageThumbnailPath,
+     @JsonKey(name: 'status') @Default('') String status,
   }) = _TvShows;
 
-  factory TvShows.fromJson(Map<dynamic,dynamic>json) => _$TvShowsFromJson(json);
+  factory TvShows.fromJson(Map<String,dynamic>json) => _$TvShowsFromJson(json);
 
 }
